71b6578a57a38871e10b53719876231b19fa4dcc
Sergey Krilov <serg.kr@gmail.com>
2020-03-15 22:42:45 -0400

    Provide additional exports to aid custom implementations
    Custom implementations may wish to reuse certain base strategies (just as R* relied on the base insert and adjust_tree strategies).

--- rtreelib/__init__.py
+++ rtreelib/__init__.py
@@ -1,3 +1,4 @@
 from rtreelib.models.rect import Rect
-from .rtree import RTreeBase, RTreeNode, RTreeEntry
-from .strategies import RTreeGuttman, RTreeGuttman as RTree, RStarTree
+from .rtree import RTreeBase, RTreeNode, RTreeEntry, DEFAULT_MAX_ENTRIES, EPSILON
+from .strategies import (
+    RTreeGuttman, RTreeGuttman as RTree, RStarTree, insert, adjust_tree_strategy, least_area_enlargement)
--- rtreelib/strategies/__init__.py
+++ rtreelib/strategies/__init__.py
@@ -1,2 +1,3 @@
 from .guttman import RTreeGuttman
 from .rstar import RStarTree
+from .base import insert, adjust_tree_strategy, least_area_enlargement
